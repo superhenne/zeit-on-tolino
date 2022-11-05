3d474c69d763a7bb80b61da88d720380c93a2240
Hendrik Koch <superhenne@gmail.com>
2022-11-05 20:31:35 +0100

    Update tolino.py

--- zeit_on_tolino/tolino.py
+++ zeit_on_tolino/tolino.py
@@ -106,7 +106,7 @@ def upload_e_paper(webdriver: WebDriver, file_path: Path, e_paper_title: str) ->
     popup_button_css = 'div[data-test-id="dialogButton-0"]'
     WebDriverWait(webdriver, Delay.small).until(EC.presence_of_element_located((By.CSS_SELECTOR, popup_button_css)))
     WebDriverWait(webdriver, Delay.small).until(EC.element_to_be_clickable((By.CSS_SELECTOR, popup_button_css)))
-    my_books_button = webdriver.find_element(By.CSS_SELECTOR, popup_button_css)
+    popup_button_css = webdriver.find_element(By.CSS_SELECTOR, popup_button_css)
     time.sleep(Delay.small)
     popup_button_css.click()
 
