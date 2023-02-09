d07acde64f45eab8b072587f274bb648ea648885
Hendrik Koch <superhenne@gmail.com>
2023-02-09 09:18:50 +0100

    Update tolino.py

--- zeit_on_tolino/tolino.py
+++ zeit_on_tolino/tolino.py
@@ -50,9 +50,11 @@ def _login(webdriver: WebDriver) -> None:
     WebDriverWait(webdriver, Delay.medium).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-test-id="ftu-countrySelection-countryList"]'))
     )
+    log.info('css: ftu-countrySelection-countryList')
     WebDriverWait(webdriver, Delay.medium).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-test-id="ftu-country-en-DE"]'))
     )
+    log.info("css: ftu-country-en-DE")
     time.sleep(Delay.small)
     for div in webdriver.find_elements(By.TAG_NAME, "div"):
         if div.text == TOLINO_COUNTRY_TO_SELECT:
