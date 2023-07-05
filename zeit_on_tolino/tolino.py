1f5fb2fbecf9c28b0d2b149114351f858477b64f
Hendrik Koch <superhenne@gmail.com>
2023-07-05 22:10:36 +0200

    Update tolino.py

--- zeit_on_tolino/tolino.py
+++ zeit_on_tolino/tolino.py
@@ -114,6 +114,7 @@ def element_exists(webdriver: WebDriver, by: str, value: str) -> bool:
 
 def _upload(webdriver: WebDriver, file_path: Path, e_paper_title: str) -> None:
     # wait until logged in
+    log.info("wait until logged in")
     WebDriverWait(webdriver, Delay.large).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'span[data-test-id="library-drawer-labelLoggedIn"]'))
     )
