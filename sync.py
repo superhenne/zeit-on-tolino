cf08d5984a425d8ad9cd4aa2fe2d0d29ce028dca
superhenne <superhenne@gmail.com>
2025-02-16 16:10:11 +0100

    Update sync.py
    add debugging

--- sync.py
+++ sync.py
@@ -1,28 +1,33 @@
 import logging
-
 from zeit_on_tolino import env_vars, epub, tolino, web, zeit
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-
 if __name__ == "__main__":
-    env_vars.verify_env_vars_are_set()
-    env_vars.verify_configured_partner_shop_is_supported()
+    try:
+        env_vars.verify_env_vars_are_set()
+        env_vars.verify_configured_partner_shop_is_supported()
 
-    log.info("logging into ZEIT premium...")
-    webdriver = web.get_webdriver()
+        log.info("logging into ZEIT premium...")
+        webdriver = web.get_webdriver()
+        
+        try:
+            # download ZEIT
+            log.info("downloading most recent ZEIT e-paper...")
+            e_paper_path = zeit.download_e_paper(webdriver)
+            e_paper_title = epub.get_epub_info(e_paper_path)["title"]
+            if not e_paper_path.is_file():
+                raise FileNotFoundError(f"Downloaded file not found: {e_paper_path}")
+            log.info(f"successfully finished download of '{e_paper_title}'")
 
-    # download ZEIT
-    log.info("downloading most recent ZEIT e-paper...")
-    e_paper_path = zeit.download_e_paper(webdriver)
-    e_paper_title = epub.get_epub_info(e_paper_path)["title"]
-    assert e_paper_path.is_file()
-    log.info(f"successfully finished download of '{e_paper_title}'")
-
-    # upload to tolino cloud
-    log.info("upload ZEIT e-paper to tolino cloud...")
-    tolino.login_and_upload(webdriver, e_paper_path, e_paper_title)
-
-    webdriver.quit()
-    log.info("done.")
+            # upload to tolino cloud
+            log.info("upload ZEIT e-paper to tolino cloud...")
+            tolino.login_and_upload(webdriver, e_paper_path, e_paper_title)
+        finally:
+            webdriver.quit()
+            log.info("WebDriver quit successfully.")
+        
+        log.info("done.")
+    except Exception as e:
+        log.error(f"An error occurred: {e}", exc_info=True)
